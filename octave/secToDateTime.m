3113ab907aaff32f420ed8ed0d4bdd0e53ea9c84
Robert Ennis <Robert.Ennis@psychol.uni-giessen.de>
2024-01-29 11:50:30 +0100

    octave does not have matlabs datetime function

--- octave/secToDateTime.m
+++ octave/secToDateTime.m
@@ -4,7 +4,9 @@ function [dt] = secToDateTime(s)
     ns_u64 = uint64(ns);
     wholeSeconds = floor(double(ns_u64)/1e9);
     fracSeconds = double(ns_u64 - uint64(wholeSeconds)*1e9)/1e9;
-    dt = datetime(wholeSeconds, 'TimeZone', 'local', 'ConvertFrom', 'posixTime', 'Format', 'yyyy.MM.dd HH:mm:ss.SSSSSSSSS') + seconds(fracSeconds);
-
+    
+    lt = localtime(wholeSeconds);
+    dt = strftime("%Y-%m-%d %X", lt);
+    
     return;
 endfunction
