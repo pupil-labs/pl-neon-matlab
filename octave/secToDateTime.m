a533a62f56dd4843680bbd17dc0be4a9070093a3
Robert Ennis <robertennis@roberts-laptop.localdomain>
2024-02-13 15:33:16 +0100

    avoid any issues with imu timestamp conversion in octave

--- octave/secToDateTime.m
+++ octave/secToDateTime.m
@@ -1,16 +1,18 @@
 % https://de.mathworks.com/matlabcentral/answers/177342-how-to-convert-nanoseconds-timestamp-to-datetime-structure
 function [dt] = secToDateTime(s)
-    ns = fix(s*1e9);
-    ns_u64 = uint64(ns);
-    wholeSeconds = floor(double(ns_u64)/1e9);
-    fracSeconds = double(ns_u64 - uint64(wholeSeconds)*1e9)/1e9;
+    % ns = fix(s*1e9);
+    % ns_u64 = uint64(ns);
+    % wholeSeconds = floor(double(ns_u64)/1e9);
+    % fracSeconds = double(ns_u64 - uint64(wholeSeconds)*1e9)/1e9;
     
-    lt = localtime(wholeSeconds);
-    lt.usec = fracSeconds * 1000000;
+    % lt = localtime(wholeSeconds);
+    % lt.usec = fracSeconds * 1000000;
     
-    dt = strftime("%Y-%m-%d %X", lt);
+    % dt = strftime("%Y-%m-%d %X", lt);
     
-    % maybe just call out to python for this?
+    % just call out to python for this.
+    % the pure octave approach is not as clean as the pure matlab approach
+    dt = py.datetime.fromtimestamp(s);
 
     return;
 endfunction
